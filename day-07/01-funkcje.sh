83790c0cf3b347d22bb02393676b7a2f26495ae7
Mateusz Adamowski <m.adamowski@alx.pl>
2022-02-19 11:32:06 +0100

    funkcje

---
+++ day-07/01-funkcje.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function powitanie {
+	echo "Witaj w programie XYZ"
+	echo "To tyle"
+}
+
+
+echo "Start programu"
+powitanie
+powitanie
+powitanie
+powitanie
+echo "Koniec programu"
+
+
